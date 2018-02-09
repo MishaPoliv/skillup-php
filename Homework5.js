0e8ce2edd9b49fba6a48033dbb00dc9f12ef2e94
MishaPoliv <xFun777@gmail.com>
2018-02-09 21:16:09 +0200

    Homework 5 (patch 1.0)

---
+++ Homework5.js
@@ -0,0 +1,53 @@
+//2e
+var c, d, result;
+c = 15;
+d = 2;
+result = c + d;
+
+alert (result);
+
+//3e
+var a, b, c, sum;
+a = 10;
+b = 2;
+c = 5;
+
+sum= a + b + c;
+
+alert (sum);
+
+//4e
+var a, b, c, d, result;
+a = 17;
+b = 10;
+c = a + b;
+
+d = 7;
+
+// 5e
+result = c + d;
+
+alert (result);
+
+//6e
+var str;
+str = ('Privet, Mir');
+
+alert (str);
+
+//7e
+var str1, str2, str7;
+str1 = 'Privet, ';
+str2 = 'Mir!';
+
+str7 = str1 + str2;
+
+alert (str7);
+
+//8e
+var name, str8;
+name = 'Misha';
+
+str8 = str1 + name;
+
+alert (str8);
