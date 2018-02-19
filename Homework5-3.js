25a5cfc4659c9fc439b967f870821ac2319c0b42
MishaPoliv <xFun777@gmail.com>
2018-02-19 21:38:03 +0200

    Homework 5, доделан

--- Homework5-3.js
+++ Homework5-3.js
@@ -12,7 +12,7 @@ user.number = '349811002';
 console.log(user);
 
 
-//                      Задание 2
+//                      Задание 2 (если с подвязкой Задания 1)
 //Добавьте в массив метод, который будет рассчитывать год рождения человека исходя из его возраста и возвращать его.
 
 var YearBirth = 2018 - user.age;
@@ -25,13 +25,6 @@ console.log(user.YearB);
 //Создайте ассоциативный массив свойство mas которое содержит массив чисел (любого количества),
 //добавьте метод, который выводит сумму чисел содержащихся в mas.
 
-
-//mas = [5, 6, 34, 12, 45, 99, 104, 3, 1];
-//var sum = 0;
-//for (var i = 0; i < mas.length; i++) {
-//    sum += mas[i]; }
-
-//    console.log(sum);
 
 var arr = {
     mas: [5, 6, 34, 12, 45, 99, 104, 3, 1]
@@ -56,30 +49,30 @@ alert('sum:' + arr.sum());
 //result.innerHTML = 'Photo: < img src="' + obj[10001].ssilka + '"> <br>'; << ili tak
 
 //Выведите массив в консоль. Выведите на страницу HTML описание первого товара.
+document.write("<br>");
 
+// !!! Почему??? .../raw/.../***.jpg !!! Если ссылка идёт вместо ".../raw/..." >>> ".../blob/..." ???
 
 var obj = {
-        10001: {name: 'креветка', price: '149.00', masa: '2 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/tree/homework5forLe5/krevetka.jpg', ready: 'Yes'},
-        51000: {name: 'Треска', price: '53.40', masa: '1 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/tree/homework5forLe5/treska.jpg', ready: 'No'},
-        89625: {name: 'Якорь', price: '2999.99', masa: '60 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/tree/homework5forLe5/yakor.jpg', ready: 'Yes'},
-        71755: {name: 'Леска', price: '70.00', masa: '0.12 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/tree/homework5forLe5/leska.jpg', ready: 'Yes'}
+        10001: {name: 'креветка', price: '149.00', masa: '2 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/raw/Homework5forLes5/krevetka.jpg', ready: 'Yes'},
+        51000: {name: 'Треска', price: '53.40', masa: '1 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/raw/Homework5forLes5/treska.jpg', ready: 'No'},
+        89625: {name: 'Якорь', price: '2999.99', masa: '60 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/raw/Homework5forLes5/yakor.jpg', ready: 'Yes'},
+        71755: {name: 'Леска', price: '70.00', masa: '0.12 kg', ssilka: 'https://github.com/MishaPoliv/skillup-php/raw/Homework5forLes5/leska.jpg', ready: 'Yes'}
     };
-
-console.log(obj);
+        console.log(obj);
 for (art in obj) {
    document.write(obj[art].name + '<br>');
    document.write(obj[art].price + '<br>');
    document.write(obj[art].masa + '<br>');
-   document.write('Photo: <img src="' + obj[art].ssilka + '"> <br>');
-   document.write(obj[art].ready + '<br>');
+   document.write('Photo: <img src=" ' + obj[art].ssilka + '" width="100" height="70" > <br>');
+   document.write('Есть на складе? = ' + obj[art].ready + '<br>');
    document.write('<hr>');
 }
 
-
-
-
 //                        Задача  5
 // Дан ассоциативный массив. Выведите его на страницу html в формате ключ — значение. Каждый элемент с новой строки.
+document.write('<br>');
+document.write('Задача 5');
 document.write('<br>');
 
 var day = {
@@ -89,20 +82,16 @@ var day = {
     "d4" : "Четверг"
 };
 
-for (var i = 1; i < 5; i++) {
-
-    document.write('<br>');
-    document.write('"' + "d"+i + '"' + ' - ');
-document.write(day["d" +i]);
-}
-
+    for (var i in day)
+        document.write(i + " - " + day[i] + "<br>");
 
 //                        Задача  6
 //Дан ассоциативный массив. Выведите его на страницу html в формате ключ *** значение. Каждый элемент с новой строки.
 document.write('<br>');
+document.write('Задача 6');
+document.write('<br>');
 
-
-var arr = {
+var arrr = {
     "k1" : "17w4",
     "k2" : "1sfsg7",
     "k3" : "17nd",
@@ -110,14 +99,9 @@ var arr = {
     "k14" : "1w37",
     "3k1" : "1wrw7"
 };
-for (var i = 1; i < 7; i++) {
 
-
-    document.write('<br>');
-    document.write('"' + "d"+i + '"' + ' *** ');
-    document.write(arr[i]);
-}
-
+for (var i in arrr)
+    document.write(i + " *** " + arrr[i] + "<br>");
 
 
 //                          Задание 7
@@ -126,7 +110,7 @@ for (var i = 1; i < 7; i++) {
 // Считайте из LocalStorage запись mass.
 // Выведите в консоль. Преобразуйте в массив и повторите вывод в консоль.
 
-{
+
     var arr = {
         "k1" : "17w4",
         "k2" : "1sfsg7",
@@ -135,12 +119,27 @@ for (var i = 1; i < 7; i++) {
         "k14" : "1w37",
         "3k1" : "1wrw7"
     };
-}
 
 console.log(arr);
 
+mass = [];
+for (var i in arr)
+mass[i] = arr[i];
+
+for (var i in arr) {
+    var localValue = localStorage.getItem(i);
+         console.log(localValue);
+}
+    console.log(mass);
+
+
 //                      Задание 8
 // Дан массив товаров, выведите товары на страницу цены которых больше 20
+document.write('<br>');
+document.write('Задача 8 - Товары с ценой "> 20"');
+
+
+
 var goods = {
     "dd" : {
         "name": "Яблоки",
@@ -164,19 +163,23 @@ var goods = {
     },
 };
 
-for (var i = 0; i < 5; i++) {
-    if (goods["cost"] > 20)
+for (var i in goods)
+    if (goods[i].cost > 20)
     {
-        for (art in goods) {
 document.write('<br>');
-document.write(goods[art].cost);
+    document.write(goods[i].name + " - " + goods[i].cost);
+
     }
-}
+
 
 
 //                          Задание 9
 // Дан массив товаров, выведите товары на страницу HTML,
 // цены которых лежат в диапазоне от 220 до 300 и количество (amount) больше 200
+document.write('<br>');document.write('<br>');
+document.write('Задача 9');
+
+
 
 var goods = {
     "apple" : {
@@ -203,15 +206,14 @@ var goods = {
         "name": "Вишни",
         "cost": 19,
         "amount":312
-    },
+    }
 };
 
-for (var i = 0; i < 5; i++) {
-    if (350 > goods["cost"] > 220);
+for (var i in goods)
+    if (350 > goods[i].cost > 220);
     {
-        document.write('<br>');
-        document.write(goods);
+      document.write('<br>');
+      document.write(goods[i].name + ("<br>") + " c ценой = ( " + goods[i].cost + " ) и кол-во = ( " + goods[i].amount + " )");
     }
-}
 
 
